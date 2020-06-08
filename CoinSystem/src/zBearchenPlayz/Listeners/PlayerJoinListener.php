36a44261ac209027f65b293ee9f6b7c6eda53543
startguy <119738343+startguy@users.noreply.github.com>
2020-06-08 10:57:25 +0000

    Fixes #139

--- CoinSystem/src/zBearchenPlayz/Listeners/PlayerJoinListener.php
+++ CoinSystem/src/zBearchenPlayz/Listeners/PlayerJoinListener.php
@@ -7,3 +7,7 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerLoginEvent;
+
+class PlayerJoinListener implements Listener{
+    
+    public function onJoin(PlayerJoinEvent){
