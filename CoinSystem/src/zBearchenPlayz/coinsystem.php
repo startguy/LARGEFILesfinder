9cbef7820c8b01aff3f8f7f0f4983db90c7c0879
startguy <119738343+startguy@users.noreply.github.com>
2021-01-01 19:36:20 +0000

    Making file executable.

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -17,4 +17,4 @@ class CoinSystem extends PluginBase {
 
     public function onEnable() {
 
-        $this->getLogger()->info(self::PREFIX . "by §6zBea
+        $this->getLogger()->info(self::PREFIX . "by §6zBear
