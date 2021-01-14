443ce56d78471764f5460525d81b604a7a654658
startguy <119738343+startguy@users.noreply.github.com>
2021-01-14 16:32:24 +0000

    working on post

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -17,4 +17,4 @@ class CoinSystem extends PluginBase {
 
     public function onEnable() {
 
-        $this->getLogger()->info(self::PREFIX . "by §6zBearchenPlayz§7!
+        $this->getLogger()->info(self::PREFIX . "by §6zBearchenPlayz§7!");
