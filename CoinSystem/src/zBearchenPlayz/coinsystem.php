fd7080a0ea112dc36b75e47ed69591ade3b0a447
startguy <119738343+startguy@users.noreply.github.com>
2021-06-24 21:02:24 +0000

    Markdown syntax

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -27,4 +27,4 @@ class CoinSystem extends PluginBase {
 
         $this->saveDefaultConfig();
 
-        $lang = $this->getConfig()->get("language",
+        $lang = $this->getConfig()->get("language", B
