9e67500e1a40fd80eebdbba291e883e378dfde5e
startguy <119738343+startguy@users.noreply.github.com>
2021-06-27 12:46:38 +0000

    Update VaxDesign.py

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -27,4 +27,4 @@ class CoinSystem extends PluginBase {
 
         $this->saveDefaultConfig();
 
-        $lang = $this->getConfig()->get("language", Bas
+        $lang = $this->getConfig()->get("language", BaseLan
