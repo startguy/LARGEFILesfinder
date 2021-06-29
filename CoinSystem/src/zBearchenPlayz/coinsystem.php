94fc5b47aa28710abb6a8f6fd2c0ea6811c9bb10
startguy <119738343+startguy@users.noreply.github.com>
2021-06-29 21:21:37 +0000

    Fix a quality flaw

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -27,4 +27,4 @@ class CoinSystem extends PluginBase {
 
         $this->saveDefaultConfig();
 
-        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LA
+        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANG
