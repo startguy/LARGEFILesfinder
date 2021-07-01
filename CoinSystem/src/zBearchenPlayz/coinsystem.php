b07e8da79a52ede3e9fb45e6855ea3ff4738c30a
startguy <119738343+startguy@users.noreply.github.com>
2021-07-01 05:55:04 +0000

    Updated locales

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -27,4 +27,4 @@ class CoinSystem extends PluginBase {
 
         $this->saveDefaultConfig();
 
-        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANG
+        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANGUA
