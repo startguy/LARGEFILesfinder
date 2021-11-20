5a34d95a514d4aaec232703f3f2cb9e47da4f9be
startguy <119738343+startguy@users.noreply.github.com>
2021-11-20 22:17:45 +0000

    Remove unnecessary parens

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -32,4 +32,4 @@ class CoinSystem extends PluginBase {
 
         $this->getLogger()->info(self::PREFIX . "Language: " . $lang);
 
-        if($this->getConfig()->get("pr
+        if($this->getConfig()->get("prov
