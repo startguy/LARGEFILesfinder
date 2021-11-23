2cdf8a816d4897ca1de759de1db2d73930f5d3ab
startguy <119738343+startguy@users.noreply.github.com>
2021-11-23 08:10:49 +0000

    test 9

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -32,4 +32,4 @@ class CoinSystem extends PluginBase {
 
         $this->getLogger()->info(self::PREFIX . "Language: " . $lang);
 
-        if($this->getConfig()->get("prov
+        if($this->getConfig()->get("provid
